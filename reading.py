20bfe1d24b89864fb74c30ac041c9faf533e540f
zeyu li <53364734+lizeyujack@users.noreply.github.com>
2022-04-30 14:00:09 +0800

    Add files via upload

--- reading.py
+++ reading.py
@@ -5,8 +5,9 @@ from cv2 import setUseOpenVX
 import torch.nn as nn
 import torch
 import numpy as np
-from keras.utils.np_utils import *
+# from keras.utils.np_utils import *
 from torch.autograd import Function
+torch.cuda.set_device(1)
 inputs_dim, featre_dim = 310, 128
 labels_dim, domain_dim = 64, 64
 labels_num, domain_num = 3, 5
@@ -23,34 +24,36 @@ class ReverseLayerF(Function):
 
 class CNNModel(nn.Module):
   def __init__(self):
+    # self.bias_class = torch.autograd.VariableVariable(torch.FloatTensor([128]),requires_grad=True)
+    # self.bias_domain = torch.autograd.VariableVariable(torch.FloatTensor([]),requires_grad=True)
     super(CNNModel, self).__init__()
     self.feature = nn.Sequential()
-    self.feature.add_module('c_fc1', nn.Linear(310, 100))
-    # self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100))
+    self.feature.add_module('c_fc1', nn.Linear(310, 128))
+    self.feature.add_module('c_bn1', nn.BatchNorm1d(128))
     self.feature.add_module('c_relu1', nn.ReLU(True))
     self.feature.add_module('c_drop1', nn.Dropout())
-    self.feature.add_module('c_fc2', nn.Linear(100, 100))
-    # self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(100))
+    self.feature.add_module('c_fc2', nn.Linear(128, 100))
+    self.feature.add_module('c_bn2', nn.BatchNorm1d(100))
     self.feature.add_module('c_relu2', nn.ReLU(True))
     self.feature.add_module('c_fc3', nn.Linear(100, 310))
     self.feature.add_module('c_softmax', nn.LogSoftmax(dim=1))
-
+    
     self.class_classifier = nn.Sequential()
-    self.class_classifier.add_module('c_fc1', nn.Linear(310, 100))
-    # self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100))
+    self.class_classifier.add_module('c_fc1', nn.Linear(310, 128))
+    self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(128))
     self.class_classifier.add_module('c_relu1', nn.ReLU(True))
     self.class_classifier.add_module('c_drop1', nn.Dropout())
-    self.class_classifier.add_module('c_fc2', nn.Linear(100, 100))
-    # self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(100))
+    self.class_classifier.add_module('c_fc2', nn.Linear(128, 100))
+    self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(100))
     self.class_classifier.add_module('c_relu2', nn.ReLU(True))
     self.class_classifier.add_module('c_fc3', nn.Linear(100, 3))
     self.class_classifier.add_module('c_softmax', nn.LogSoftmax(dim=1))
 
     self.domain_classifier = nn.Sequential()
-    self.domain_classifier.add_module('d_fc1', nn.Linear(310, 100))
-    # self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
+    self.domain_classifier.add_module('d_fc1', nn.Linear(310, 128))
+    self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(128))
     self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
-    self.domain_classifier.add_module('d_fc2', nn.Linear(100, 5))
+    self.domain_classifier.add_module('d_fc2', nn.Linear(128, 5))
     self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
 
   def forward(self, input_data,alpha):
@@ -59,6 +62,7 @@ class CNNModel(nn.Module):
     # print('shushuben\t',input_data.shape)
     reverse_feature = ReverseLayerF.apply(feature, alpha)
     class_output = self.class_classifier(feature)
+    
     domain_output = self.domain_classifier(reverse_feature)
 
     return class_output, domain_output
@@ -67,18 +71,19 @@ class DANN:
   def __init__(self,lr = 1e-3,batch_size = 128,image_size = 28,n_epoch = 100):
     with open('./data.pkl', 'rb') as f:
       self.data = pickle.load(f)
-    self.my_net = CNNModel()
+    self.my_net = CNNModel().cuda()
     self.optimizer = torch.optim.SGD(self.my_net.parameters(), lr=lr)
-    self.loss_class = torch.nn.MSELoss()
-    self.loss_domain = torch.nn.MSELoss()
+    self.loss_class = torch.nn.MSELoss().cuda()
+    self.loss_domain = torch.nn.MSELoss().cuda()
 
   def acc_rate(self,predict,labels):
     counter = 0
+    zeross = torch.zeros(1,predict.shape[1]).cuda()
     for i,j in zip(predict,labels):
-      temp = torch.zeros(1,labels.shape[1])
+      temp = torch.zeros(1,labels.shape[1]).cuda()
       temp[:,int(i.argmax(dim=0))] = 1
       # print(temp*j)
-      if torch.zeros(1,3).equal(temp*j):
+      if zeross.equal(temp*(j.cuda())):
         pass
       else:
         counter += 1
@@ -100,6 +105,7 @@ class DANN:
   def test(self,test):
     # to do
     t_img,t_label,valid_d=test
+    t_img ,t_label,valid_d = t_img.cuda() ,t_label.cuda(),valid_d.cuda()
     p = float(len(t_label)*len(t_label))
     alpha = 2. / (1. + np.exp(-10 * p)) - 1
     class_output, valid_output = self.my_net(t_img.to(torch.float32), alpha)
@@ -109,6 +115,8 @@ class DANN:
 
   def train(self,n_epoch,batch_size,train,test,folder):
     s_img, s_label,domain_label = train
+    # print(domain_label.shape,'\n',s_label.shape),s_img.shape,s_label.shape)
+    # sys.exit()
     t_img,t_label,domain_t = test
     for epoch in range(n_epoch):
       index_batch = 0
@@ -119,30 +127,31 @@ class DANN:
         self.my_net.zero_grad()
 
         if ((index_batch+1)*batch_size < len(s_label)):
-          s_img_mini = s_img[index_batch*batch_size:(index_batch+1)*batch_size,:]
-          s_label_mini = s_label[index_batch*batch_size:(index_batch+1)*batch_size,:]
-          domain_label_mini =domain_label[folder*3397+index_batch*batch_size:folder*3397+(index_batch+1)*batch_size,:]
-        else:
-          s_img_mini = s_img[index_batch*batch_size:,:]
-          s_label_mini = s_label[index_batch*batch_size:,:]
-          domain_label_mini =domain_label[folder*3397+index_batch*batch_size:,:]
-
-        
+          s_img_mini = s_img[index_batch*batch_size:(index_batch+1)*batch_size,:].cuda()
+          s_label_mini = s_label[index_batch*batch_size:(index_batch+1)*batch_size,:].cuda()
+          domain_label_mini =domain_label[index_batch*batch_size:(index_batch+1)*batch_size,:].cuda()
+          # print('\n\nid_num+index_batch*batch_size: \n\n',id_num+index_batch*batch_size,'\n',id_num+(index_batch+1)*batch_size)
+        elif((index_batch)*batch_size <= len(s_label)):
+          s_img_mini = s_img[index_batch*batch_size:,:].cuda()
+          s_label_mini = s_label[index_batch*batch_size:,:].cuda()
+          domain_label_mini =domain_label[index_batch*batch_size:,:].cuda()
+          # print('\n\nid_num+index_batch*batch_size: \n\n',id_num+index_batch*batch_size,'\n',id_num+index_batch*batch_size+len(s_label_mini))
+        # print(s_img_mini.shape)
         class_output, domain_output = self.my_net(s_img_mini.to(torch.float32),alpha)
         # print('classes\t',class_output.shape)
         # print('domain_output\t',class_output.shape,s_label_mini.shape)
         # sys.exit()
-
+        # print(domain_label_mini.shape,s_label_mini.shape,s_img_mini.shape)
         err_s_label = self.loss_class(class_output.to(torch.float32), s_label_mini.to(torch.float32))
         err_s_domain = self.loss_domain(domain_output.to(torch.float32), domain_label_mini.to(torch.float32))
 
-        if ((index_batch%4+1)*batch_size < len(s_label)):
-          t_img_mini = t_img[index_batch*batch_size:(index_batch+1)*batch_size,:]
-          domain_t_label_mini = domain_t[index_batch*batch_size:(index_batch+1)*batch_size,:]
+        if ((index_batch+1)*batch_size < len(s_label)):
+          t_img_mini = t_img[int(index_batch*batch_size/4):int((index_batch+1)*batch_size/4),:].cuda()
+          domain_t_label_mini = domain_t[int(index_batch*batch_size/4):int((index_batch+1)*batch_size/4),:].cuda()
 
-        elif((index_batch%4)*batch_size <= len(s_label)):
-          t_img_mini = t_img[index_batch*batch_size:,:]
-          domain_t_label_mini = domain_t[index_batch*batch_size:,:]
+        elif((index_batch)*batch_size <= len(s_label)):
+          t_img_mini = t_img[int(index_batch*batch_size/4):,:].cuda()
+          domain_t_label_mini = domain_t[int(index_batch*batch_size/4):,:].cuda()
         
         # print('t_img_mini\t',t_img_mini.shape)
 
@@ -173,7 +182,7 @@ class DANN:
   def run(self):
     for folder in range(domain_num):
       train, test = self.fetch_data(folder)
-      self.train(50,400,train,test, folder)
+      self.train(10,400,train,test, folder)
       self.test(test)
       # self.test_init(test)
 
